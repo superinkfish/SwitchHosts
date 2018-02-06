738f7d34ffa845bca00dca8db7dac4ae5e8a1c86
激酶 <qiming.wqm@alibaba-inc.com>
2018-02-06 15:07:51 +0800

    update getUrl.js

--- app/server/actions/getUrl.js
+++ app/server/actions/getUrl.js
@@ -5,21 +5,33 @@
 
 'use strict'
 
-const request = require('request')
+const request = require('request');
+const fs = require('fs');
 //process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 
 module.exports = (svr, url) => {
   return new Promise((resolve, reject) => {
-    request({
-      url,
-      rejectUnauthorized: false
-    }, (err, res, body) => {
-      if (err) {
-        console.log(err)
-        reject(err)
-      } else {
-        resolve(body)
+    if(!url.startsWith('http')){
+      try {
+        var body = fs.readFileSync(url).toString();
+        resolve(body);
+      } catch (e) {
+        console.log(url)
+        console.log(e)
+        reject(e)
       }
-    })
+    } else {
+      request({
+        url,
+        rejectUnauthorized: false
+      }, (err, res, body) => {
+        if (err) {
+          console.log(err)
+          reject(err)
+        } else {
+          resolve(body)
+        }
+      });
+    }
   })
 }
